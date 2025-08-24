e3c2dc21a9f7d8dd2d0f5c90edd117cd0a16eeee
Dev Eyes <163506644+DevEyssuka@users.noreply.github.com>
2025-08-24 10:38:19 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/deveyesmainsite.github.io/", // чтобы GitHub Pages видел правильно
+  base: "/deveyesmainsite.github.io/",
 })
