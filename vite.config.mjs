e9132a63b90b0fd9dcfcf269d7c45051b2f77d2c
Dev Eyes <163506644+DevEyssuka@users.noreply.github.com>
2025-08-24 10:34:48 +0300

    Update and rename vite.config.js to vite.config.mjs

---
+++ vite.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/deveyesmainsite.github.io/", // чтобы GitHub Pages видел правильно
+})
